1fb03484d0285321316a5f8b2c9df567753d47ab
ANDRIOD\Android <kriteshmaurya91@gmail.com>
2018-07-03 19:14:53 +0530

    change commit

--- app/src/main/java/com/cfcs/komaxcustomer/SplashActivity.java
+++ app/src/main/java/com/cfcs/komaxcustomer/SplashActivity.java
@@ -2,13 +2,18 @@ package com.cfcs.komaxcustomer;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.widget.ProgressBar;
 
 import com.cfcs.komaxcustomer.config_customer.Config_Customer;
@@ -16,6 +21,7 @@ import com.cfcs.komaxcustomer.customer_activity.DashboardActivity;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
@@ -32,12 +38,33 @@ public class SplashActivity extends Activity {
     public ProgressBar progressBar;
     private static int SPLASH_TIME_OUT = 1500;
 
+    String currentVersion = null;
+
+    String newVersion = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         progressBar = findViewById(R.id.progressBar_splash);
+
+
+        try {
+            currentVersion = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Config_Customer.isOnline(SplashActivity.this);
+        if (Config_Customer.internetStatus == true) {
+
+            new GetVersionCode().execute();
+
+        } else {
+            Config_Customer.toastShow("No Internet Connection! Please Reconnect Your Internet", SplashActivity.this);
+            finish();
+        }
 
 
         new Handler().postDelayed(new Runnable() {
@@ -57,6 +84,56 @@ public class SplashActivity extends Activity {
 
             }
         }, SPLASH_TIME_OUT);
+    }
+
+
+    private class GetVersionCode extends AsyncTask<Void, String, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                newVersion = Jsoup.connect("https://play.google.com/store/apps/details?id=" + SplashActivity.this.getPackageName() + "&hl=it")
+                        .timeout(30000)
+                        .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                        .referrer("http://www.google.com")
+                        .get()
+                        .select("div[itemprop=softwareVersion]")
+                        .first()
+                        .ownText();
+                return newVersion;
+            } catch (Exception e) {
+                return newVersion;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String onlineVersion) {
+            super.onPostExecute(onlineVersion);
+            if (onlineVersion != null && !onlineVersion.isEmpty()) {
+                if (Float.valueOf(currentVersion) < Float.valueOf(onlineVersion)) {
+                    //show dialog
+                    showForceUpdateDialog();
+                }
+            }
+            Log.d("update", "Current version " + currentVersion + "playstore version " + onlineVersion);
+        }
+
+    }
+
+    public void showForceUpdateDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(new ContextThemeWrapper(SplashActivity.this,
+                R.style.AppTheme));
+
+        alertDialogBuilder.setTitle(SplashActivity.this.getString(R.string.youAreNotUpdatedTitle));
+        alertDialogBuilder.setMessage(SplashActivity.this.getString(R.string.youAreNotUpdatedMessage) + " " + newVersion + SplashActivity.this.getString(R.string.youAreNotUpdatedMessage1));
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton(R.string.update1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                SplashActivity.this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + SplashActivity.this.getPackageName())));
+                dialog.cancel();
+            }
+        });
+        alertDialogBuilder.show();
     }
 
     public class LoginCheck extends AsyncTask<String, String, String> {
